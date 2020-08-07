d025dc3613a71d1eaa6a022ac84d0a23578efbcf
Sophoana <sophoap@amazon.com>
2020-08-07 16:38:46 -0500

    Added ProductArray.py

---
+++ ProductArray.py
@@ -0,0 +1,20 @@
+# Product of an array except itself
+class Solution:
+    def productExceptSelf(self, nums):
+        right = [1] * len(nums)
+        prod = 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            prod *= nums[i+1]
+            right[i] = prod
+
+        prod = 1
+        for i in range(1, len(nums)):
+            prod *= nums[i-1]
+            right[i] *= prod
+
+        return right
+
+
+print(Solution().productExceptSelf([1, 2, 3, 4]))
+# [24, 12, 8, 6]
